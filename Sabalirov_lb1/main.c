b829aa0b518c46e5ee3cc5635f2dbb3b33b5e879
Мухаммад Сабалиров <60626980+MikelazDotPy@users.noreply.github.com>
2025-02-13 22:46:45 +0300

    Дебаг мод

--- Sabalirov_lb1/main.c
+++ Sabalirov_lb1/main.c
@@ -1,17 +1,27 @@
-#include "stdio.h"
 #include "malloc.h"
+#include "stdio.h"
 #include "string.h"
+#include "time.h"
 
-#define MAX_ASSESMENT 44
-#define MAX_N 40
+#define ASSESSMENT_ARR {N + 4, N / 2 + 7, N / 3 + 9, N / 4 + 10, N / 5 + 11, N / 6 + 12}
 #define ASSESSMENT_MISTAKE 3
 #define ASSESSMENTS_COUNT 6
-#define ASSESSMENT_ARR {N + 4, N / 2 + 7, N / 3 + 9, N / 4 + 10, N / 5 + 11, N / 6 + 12}
+#define DEBUG 1
+#define MAX_ASSESMENT 44
+#define MAX_N 40
 
-#define MIN(a, b) (a < b ? a : b)
-#define MAX(a, b) (a > b ? a : b)
-#define INIT_FOR(y1, y2, x) (y1 == y2 ? x : 0)
 #define GET_SQUARE_ROW(info, x, side) ((info.R >> (info.n - side)) << (info.n - x - side))
+#define INIT_FOR(y1, y2, x) (y1 == y2 ? x : 0)
+#define MAX(a, b) (a > b ? a : b)
+#define MIN(a, b) (a < b ? a : b)
+
+
+typedef struct {
+    const size_t N;
+    const size_t square;
+    const size_t R;
+    const size_t n;
+} info_t;
 
 
 typedef struct {
@@ -20,12 +30,6 @@ typedef struct {
     size_t side;
 } square_t;
 
-typedef struct {
-    const size_t N;
-    const size_t square;
-    const size_t R;
-    const size_t n;
-} info_t;
 
 typedef struct {
     size_t curr_square;
@@ -38,8 +42,12 @@ typedef struct {
 
 
 size_t get_best_limit(size_t N, size_t* best_side){
+    if (DEBUG)
+        printf("Вычисление оптимальной стороны и длины решения\n");
     if (N % 2 == 0){
         *best_side = N / 2;
+        if (DEBUG)
+            printf("Сторона: %ld Длина: %d\n", *best_side, 4);
         return 4;
     }
 
@@ -48,7 +56,12 @@ size_t get_best_limit(size_t N, size_t* best_side){
             size_t j = N / i;
             *best_side = j*(i / 2 + 1);
             const size_t low_side = *best_side - j;
-            return ((*best_side) / low_side)*2 + 2 + (low_side / (*best_side - low_side))*2;
+            size_t limit = ((*best_side) / low_side)*2 + 2;
+            if (*best_side % low_side != 0)
+                limit += (low_side / (*best_side - low_side))*2;
+            if (DEBUG)
+                printf("Сторона: %ld Длина: %ld\n", *best_side, limit);
+            return limit;
         }
     }
 
@@ -58,6 +71,8 @@ size_t get_best_limit(size_t N, size_t* best_side){
     size_t assessment = assessments[0];
     for (size_t i = 1; i < ASSESSMENTS_COUNT; ++i)
         assessment = MIN(assessment, assessments[i]); 
+    if (DEBUG)
+        printf("Сторона: %ld Длина: %ld\n", *best_side, assessment - ASSESSMENT_MISTAKE);
     return assessment - ASSESSMENT_MISTAKE;
 }
 
@@ -83,6 +98,10 @@ void output_solution(square_t* squares, size_t len, size_t delta_n){
 
 
 void solution(const size_t N){
+    if (DEBUG)
+        printf("Инициализация\n");
+    size_t it = 0;
+    size_t all_it = 0;
     size_t best_side, square;
     size_t limit = get_best_limit(N, &best_side);
     const info_t info = {N, N*N, (2 << (best_side - 1)) - 1, best_side};
@@ -110,21 +129,32 @@ void solution(const size_t N){
 
         top = 0;
         while (top >= 0){
+            if (DEBUG)
+                ++it;
             curr_comb = stack[top--];
+            if (DEBUG && it % 10000 == 0)
+                printf("Проверка комбинации номер %ld: Длина %ld. Площадь %ld \n", it, curr_comb.len, curr_comb.curr_square);
             if (curr_comb.curr_square == info.square){
+                if (DEBUG)
+                    printf("Найденно решение. Проверено комбинаций: %ld Всего итераций: %ld\n", it, all_it);
                 output_solution(curr_comb.squares, curr_comb.len, (N - best_side));
                 return;
             }
 
-            if (curr_comb.len == limit)
+            if (curr_comb.len == limit){
                 continue;
+            }
 
             comb = curr_comb;
             for (size_t y = comb.next_y; y < info.n; ++y){
                 for (size_t x = INIT_FOR(y, comb.next_y, comb.next_x); x < info.n; ++x){
                     for (size_t max_side = info.n - MAX(x, y); max_side > 0; --max_side){
+                        if (DEBUG)
+                            all_it++;
                         if (can_place(curr_comb.matrix[y], info, x, max_side)){
                             for (size_t side = 1; side <= max_side; ++side){
+                                if (DEBUG)
+                                    all_it++;
                                 comb.squares[comb.len++] = (square_t){x, y, side};
                                 comb.curr_square += side*side;
                                 square = GET_SQUARE_ROW(info, x, side);
@@ -142,6 +172,8 @@ void solution(const size_t N){
             }
             next:;
         }
+        if (DEBUG)
+            printf("Решение с длиной %ld не найдено. Увеличение длины решения до %ld\n", limit, limit + 1);
         ++limit;
         memset(stack, 0, mem_size*sizeof(combination_t));
     }
@@ -151,7 +183,16 @@ void solution(const size_t N){
 
 int main(){
     size_t N;
-    scanf("%ld", &N);   
-    solution(N);
+    scanf("%ld", &N);  
+
+    clock_t t = clock(); 
+
+    solution(N); 
+
+    t = clock() - t; 
+    double time_taken = ((double)t)/CLOCKS_PER_SEC;
+    if (DEBUG)
+        printf("Выполнение заняло %f секунд \n", time_taken); 
+
     return 0;
 }
