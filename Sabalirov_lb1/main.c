b08aaeb68d50d90e516f41cc31d29acc4f2573b2
Мухаммад Сабалиров <60626980+MikelazDotPy@users.noreply.github.com>
2025-02-13 22:03:53 +0300

    Update main.c

--- Sabalirov_lb1/main.c
+++ Sabalirov_lb1/main.c
@@ -1,5 +1,6 @@
 #include "stdio.h"
 #include "malloc.h"
+#include "string.h"
 
 #define MAX_ASSESMENT 44
 #define MAX_N 40
@@ -7,10 +8,10 @@
 #define ASSESSMENTS_COUNT 6
 #define ASSESSMENT_ARR {N + 4, N / 2 + 7, N / 3 + 9, N / 4 + 10, N / 5 + 11, N / 6 + 12}
 
-#define MIN(a, b) ((a) < (b) ? (a) : (b))
-#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) (a < b ? a : b)
+#define MAX(a, b) (a > b ? a : b)
 #define INIT_FOR(y1, y2, x) (y1 == y2 ? x : 0)
-#define GET_SQUARE_ROW(info, x, side) (((info.R >> (info.n - side)) << (info.n - x - side)))
+#define GET_SQUARE_ROW(info, x, side) ((info.R >> (info.n - side)) << (info.n - x - side))
 
 
 typedef struct {
@@ -20,10 +21,10 @@ typedef struct {
 } square_t;
 
 typedef struct {
-    size_t N;
-    size_t square;
-    size_t R;
-    size_t n;
+    const size_t N;
+    const size_t square;
+    const size_t R;
+    const size_t n;
 } info_t;
 
 typedef struct {
@@ -36,9 +37,7 @@ typedef struct {
 } combination_t;
 
 
-size_t get_best_limit(size_t N, size_t* best_side, size_t* is_prime){
-    *is_prime = 0;
-
+size_t get_best_limit(size_t N, size_t* best_side){
     if (N % 2 == 0){
         *best_side = N / 2;
         return 4;
@@ -48,15 +47,14 @@ size_t get_best_limit(size_t N, size_t* best_side, size_t* is_prime){
         if (N % i == 0){
             size_t j = N / i;
             *best_side = j*(i / 2 + 1);
-            size_t low_side = *best_side - j;
+            const size_t low_side = *best_side - j;
             return ((*best_side) / low_side)*2 + 2 + (low_side / (*best_side - low_side))*2;
         }
     }
 
-    *is_prime = 1;
     *best_side = N / 2 + 1;
 
-    size_t assessments[ASSESSMENTS_COUNT] = ASSESSMENT_ARR;
+    const size_t assessments[ASSESSMENTS_COUNT] = ASSESSMENT_ARR;
     size_t assessment = assessments[0];
     for (size_t i = 1; i < ASSESSMENTS_COUNT; ++i)
         assessment = MIN(assessment, assessments[i]); 
@@ -65,11 +63,8 @@ size_t get_best_limit(size_t N, size_t* best_side, size_t* is_prime){
 
 
 int can_place(size_t row, info_t info, size_t x, size_t side){
-    size_t b = (row) ^ (((info.R >> (info.n - side)) << (info.n - x - side)));
-    size_t c = (row) | (((info.R >> (info.n - side)) << (info.n - x - side)));
-    if (b == c)
-        return 1;
-    return 0;
+    size_t square = GET_SQUARE_ROW(info, x, side);
+    return (row | square) == (row ^ square);
 }
 
 
@@ -87,33 +82,35 @@ void output_solution(square_t* squares, size_t len, size_t delta_n){
 }
 
 
-void solution(size_t N){
-    size_t best_side, is_prime;
-    size_t limit = get_best_limit(N, &best_side, &is_prime);
-    info_t info = {N, N*N, (2 << (best_side - 1)) - 1, best_side};
-    size_t init_square = best_side*best_side + 2*(N - best_side)*(N - best_side);
-    size_t init_conditions_side = 2*best_side - N;
-    size_t init_conditions = GET_SQUARE_ROW(info, 0, init_conditions_side);
-    square_t pants[3] = {
-        (square_t){0, 0, best_side},
-        (square_t){0, best_side, N - best_side},
-        (square_t){best_side, 0, N - best_side}
+void solution(const size_t N){
+    size_t best_side, square;
+    size_t limit = get_best_limit(N, &best_side);
+    const info_t info = {N, N*N, (2 << (best_side - 1)) - 1, best_side};
+    const size_t init_square = best_side*best_side + 2*(N - best_side)*(N - best_side);
+    const size_t init_conditions_side = 2*best_side - N;
+    const size_t init_conditions = GET_SQUARE_ROW(info, 0, init_conditions_side);
+    const square_t pants[3] = {
+        (const square_t){0, 0, best_side},
+        (const square_t){0, best_side, N - best_side},
+        (const square_t){best_side, 0, N - best_side}
     };
-
+    combination_t curr_comb, comb;
+    const size_t mem_size = N*N;
+    combination_t* stack = calloc(mem_size, sizeof(combination_t));
+    int top;
     while (1){
-        combination_t* stack = calloc(N*N, sizeof(combination_t));
         for (size_t i = 0; i < 3; ++i)
             stack[0].squares[i] = pants[i];
         for (size_t y = 0; y < init_conditions_side; ++y)
             stack[0].matrix[y] |= init_conditions;
         stack[0].len = 3;
         stack[0].curr_square = init_square;
-        stack[0].next_x = 0;
+        stack[0].next_x = init_conditions_side;
         stack[0].next_y = 0;
 
-        int top = 0;
+        top = 0;
         while (top >= 0){
-            combination_t curr_comb = stack[top--];
+            curr_comb = stack[top--];
             if (curr_comb.curr_square == info.square){
                 output_solution(curr_comb.squares, curr_comb.len, (N - best_side));
                 return;
@@ -122,7 +119,7 @@ void solution(size_t N){
             if (curr_comb.len == limit)
                 continue;
 
-            combination_t comb = curr_comb;
+            comb = curr_comb;
             for (size_t y = comb.next_y; y < info.n; ++y){
                 for (size_t x = INIT_FOR(y, comb.next_y, comb.next_x); x < info.n; ++x){
                     for (size_t max_side = info.n - MAX(x, y); max_side > 0; --max_side){
@@ -130,8 +127,9 @@ void solution(size_t N){
                             for (size_t side = 1; side <= max_side; ++side){
                                 comb.squares[comb.len++] = (square_t){x, y, side};
                                 comb.curr_square += side*side;
+                                square = GET_SQUARE_ROW(info, x, side);
                                 for (size_t i = y; i < side + y; ++i)
-                                    comb.matrix[i] |= GET_SQUARE_ROW(info, x, side);
+                                    comb.matrix[i] |= square;
                                 comb.next_x = x + 1;
                                 comb.next_y = y;
                                 stack[++top] = comb;
@@ -145,7 +143,7 @@ void solution(size_t N){
             next:;
         }
         ++limit;
-        free(stack);
+        memset(stack, 0, mem_size*sizeof(combination_t));
     }
 
 }
